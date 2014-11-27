c19d58f3086f77a96a3c5917b8df6ce1223a6111
Tero Teelahti <tero@teelahti.fi>
2014-11-27 16:32:44 +0200

    Randomize

--- bingo.js
+++ bingo.js
@@ -1,26 +1,36 @@
-(function() {
+(function () {
     "use strict";
 
     var words = [
-    "Staattinen tyypitys",
-    "Microservices",
-    "Haskell",
-    "Clojure",
-    "Lisp",
-    "ES6",
-    "Algebraiset tietotyypit",
-    "HATEOAS",
-    "Sivuvaikutus"
+            "Staattinen tyypitys",
+            "Microservices",
+            "Haskell",
+            "Clojure",
+            "Lisp",
+            "ES6",
+            "Algebraiset tietotyypit",
+            "HATEOAS",
+            "Sivuvaikutus"
     ],
-    template = Handlebars.default.compile(document.getElementById("grid-template").innerHTML),
-    gridInstance = {
-        words: words.map(function(word) {
-            return { selected: false, word: word };
-        })
-    },
+        template = Handlebars.default.compile(document.getElementById("grid-template").innerHTML),
+        gridInstance = {
+            words: shuffleArray(words)
+        },
     container = document.getElementById("gridContainer");
 
-  container.innerHTML = template(gridInstance);
+    container.innerHTML = template(gridInstance);
 
+
+
+
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
 
 })();
