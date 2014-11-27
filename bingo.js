55e2d68c92a230b1dec42f001ec34947918ba59e
Tuomas Hietanen <tuomas.hietanen@basware.com>
2014-11-27 18:43:21 +0200

    generating static nonsense

--- bingo.js
+++ bingo.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var gridSize = 3;
-    var words = [
+    var wordsFuture = [
         "Staattinen tyypitys",
         "Microservices",
         "Haskell",
@@ -10,28 +10,52 @@
         "Lisp",
         "ES6",
         "Algebraiset tietotyypit",
-        "HATEOAS",
-        "Sivuvaikutus",
         "REST",
         "Eventually consistent",
         "Bus",
         "Messaging",
-        "Broker",
         "Monadi",
-        "Vuotava abstraktio",
-        "Monoliittinen",
-        "SOA",
         "Reaktiivinen",
-        "Python",
         "Hajautettu",
         "Ei-rikkova",
         "Kompositio",
         "Higher-order function"
     ];
+    var wordsLastSeasonAndNegative = [
+        "Sivuvaikutus",
+        "Broker",
+        "Vuotava abstraktio",
+        "Monoliittinen",
+        "SOA",
+        "Python",
+        "HATEOAS",
+	]
 
+	var words = wordsFuture.concat(wordsLastSeasonAndNegative);
+	var suffledGood = shuffleArray(wordsFuture);
+	var suffledBad = shuffleArray(wordsLastSeasonAndNegative);
+	
     var template = Handlebars.default.compile(document.getElementById("grid-template").innerHTML),
         gridInstance = {
-            words: shuffleArray(words).slice(0, gridSize*gridSize)
+            words: shuffleArray(words).slice(0, gridSize*gridSize),
+			nonsense: 
+				"Nyt mennään vähän off-topiciksi, mutta järjestelmän pitäisi olla suunniteltu niin, että se on " + suffledGood[0].toLowerCase() + ". \
+				Lähtökohtaisesi kannattaa fokusoitua siihen, että tulevaisuudessa kaiken ydin on " + suffledGood[1].toLowerCase()  + ".\
+				Nykyisin usein sorrumme siihen, että järjestelmästä tulee aivan liian " + suffledBad[1].toLowerCase() + ".\
+				" + suffledBad[2] + " on joskus paikallaan, mutta meidän arkkitehtuurissa sitä on käytetty liikaa ja turhaan. \
+				Jokainen projekti/tiimi yrittää olla kovin " + suffledGood[2] + ", mutta sitten jälkikäteen todetaan, \
+				että koko codebase on jälleen yksi iso " + suffledBad[1].toLowerCase() + ". \
+				Asian pihvi on, että jatkossa meidän on otettava oppia termistä " + suffledGood[3] + ". \
+				Olemme unohtaneet sen, sillä kustannuksella, että järjestelmästä tuli " + suffledGood[4].toLowerCase() + " (joka on kyllä tärkeää, mutta ei itse tarkoitus).\
+				Pian meillä on miljoonien rivien codebase ja sen on oltava täysin " + suffledGood[5].toLowerCase() + ".\
+				Sillä jos jatkamme nykyistä linjaa, lopputuloksena on " + suffledBad[3].toLowerCase() + ", \
+				joka ei laadullisesti ole edes etäisesti " + suffledGood[6].toLowerCase() + ". \
+				Arkkitehtuuria on kehitettävä sellaiseen suuntaan, että voimme oikeasti jonain päivänä sanoa meillä olevan " + suffledGood[7].toLowerCase() + "-systeemi. \
+				" + suffledBad[3] + "-arkkitehtuurin on nyt vaihdettava olemaan " + suffledGood[8].toLowerCase() + "-arkkitehtuuri ja tähän on herättävä ajoissa. \
+				On itsestään selvää, että mitä enemmän " + suffledGood[9].toLowerCase() + "-kokonaisuuksia pystymme toimittamaan, niin sitä paremmin varmistamme \
+				pilvipalvelumme laadun kohti palvelua ylläpidettävämmäksi, testattavammaksi, luotettavammaksi, virhesietoisemmaksi, saatavammaksi ja kestävämmäksi \
+				kokonaisuudeksi."
+				
         },
         container = document.getElementById("gridContainer");
 
